c440ddd0a2fbcacc0bd79ef9c073dcb6931992dc
Vlad Spalnacan <spalnacan.vlad@gmail.com>
2022-06-20 16:16:02 +0300

    test build v2 mobile only

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import '../src/CSS/style.css'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { HashRouter as Router, Routes, Route } from 'react-router-dom'
 import Header from './components/Header'
 import SpecialOffers from './components/SpecialOffers'
 import Contact from './components/Contact'
